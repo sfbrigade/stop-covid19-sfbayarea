5c5c952af094741ae0e1bd265a2c7c4c8625d8d8
Thuong Ho <thuong.t.ho@gmail.com>
2020-04-30 15:07:44 -0700

    Style Side Navigation, add update hash behavior #63

--- components/Faq.vue
+++ components/Faq.vue
@@ -3,13 +3,22 @@
     <h2 class="Parent-Heading">
       <!-- Tab TBD -->
     </h2>
-    <SideNavigationOverview :categories="items" />
-    <div v-for="(item, i) in items" :key="i">
-      <FaqCategory
-        :title="item.title"
-        :last-updated-at="item.lastUpdatedAt"
-        :qa="item.qa"
-      />
+    <SideNavigationOverview :categories="items" @clicked="scrollToCategory" />
+    <div class="FaqContent-Scroll-Area">
+      <div class="FaqContent-Wrapper">
+        <div
+          v-for="(item, i) in items"
+          :id="`faq-content-${i}`"
+          :key="i"
+          :ref="i"
+        >
+          <FaqCategory
+            :title="item.title"
+            :last-updated-at="item.lastUpdatedAt"
+            :qa="item.qa"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,20 +37,49 @@ export default {
       items: Faq.faqItems
     }
   },
+  methods: {
+    scrollToCategory(category) {
+      const elem = this.$refs[category]
+      const top = elem[0].offsetTop
+      // this.$scrollTo(elem[0])
+      // const element = document.getElementById('faq-content-1')
+      // window.scrollTo({ top, behavior: 'smooth' })
+      // VueScrollTo(elem[0])
+      // const hash = `#faq-content-${category}`
+      // document.location.href = `#faq-content-${category}`
+      // window.scrollTo({ top, left: 0, behavior: 'smooth' })
+      location.hash = `#faq-content-${category}`
+
+      // eslint-disable-next-line no-console
+      console.log(category, elem[0], top)
+    }
+  },
   head() {
     return {
       title: 'General FAQ'
     }
+  },
+  transition: {
+    name: 'test',
+    mode: 'out-in'
   }
 }
 </script>
 <style lang="scss">
 .Parent {
+  display: flex;
   &-Heading {
     @include font-size(30);
     font-weight: normal;
     color: $gray-2;
     margin-bottom: 12px;
+  }
+  .FaqContent-Scroll-Area {
+    overflow-x: scroll;
+    height: calc(100vh - 80px);
+    .FaqContent-Wrapper {
+      height: 200%;
+    }
   }
 }
 a {
--- components/SideNavigationOverview.vue
+++ components/SideNavigationOverview.vue
@@ -5,11 +5,13 @@
         v-for="(item, i) in categories"
         :key="i"
         v-ripple="false"
+        :to="`#faq-content-${i}`"
         class="SideNavigationOverview-Container"
       >
         <v-list-item-content class="SideNavigationOverview-TextContainer">
           <v-list-item-title
             class="SideNavigationOverview-Text"
+            @click="viewCategory(item, i)"
             v-text="item.title"
           />
         </v-list-item-content>
@@ -25,13 +27,61 @@ export default {
       type: Array,
       required: true
     }
+  },
+  methods: {
+    viewCategory(item, i) {
+      // eslint-disable-next-line no-console
+      console.log(this.$route, item.title, i)
+      this.$emit('clicked', i)
+    }
   }
 }
 </script>
 <style lang="scss">
 .SideNavigationOverview {
+  max-width: 216px;
+  @include lessThan($small) {
+    display: none;
+  }
   &-List {
-    background: $gray-5 !important;
+    background: $white-2 !important;
+    margin: 0 1.5rem 0 1rem;
+    border-radius: 10px;
+    padding: 8px 18px;
+    a {
+      margin: 5px 0;
+    }
+    .SideNavigationOverview-TextContainer {
+      max-width: 180px;
+      min-width: 125px;
+      padding: 5px;
+      .v-list-item__title {
+        @include font-size(14);
+        text-overflow: unset;
+        white-space: break-spaces;
+      }
+    }
+
+    a:hover {
+      text-decoration: none !important;
+      .v-list-item__title {
+        color: $purple-2 !important;
+      }
+      &.v-list-item--active {
+        cursor: default;
+        .v-list-item__title {
+          color: $black !important;
+        }
+      }
+    }
+    .v-list-item--active {
+      color: $black !important;
+      font-weight: bold;
+      border-left: 5px solid $purple-1;
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+      padding-left: 10px;
+    }
   }
 }
 </style>
