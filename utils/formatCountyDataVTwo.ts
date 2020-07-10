168daa0ad9d2db20ffdd0dec06aea28b7089bc70
Thuong Ho <thuong.t.ho@gmail.com>
2020-07-09 23:04:52 -0700

    Update naming conventions, add custom options for chartjs in horizontalbar chart

--- utils/formatCountyDataVTwo.ts
+++ utils/formatCountyDataVTwo.ts
@@ -1,67 +1,88 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable camelcase */
-type AgeGraphDatasetType = {
+export enum ChartTypes {
+  AGE = 'CHART-AGE',
+  GENDER = 'CHART-GENDER'
+}
+
+const PURPLE_MAIN = '#473A8C'
+
+type AgeDataset = {
   backgroundColor: string
   data: Array<number>
 }
 
-type AgeGroupGraphType = {
-  datasets: Array<AgeGraphDatasetType>
+type AgeGroup = {
+  chartType: string
+  datasets: Array<AgeDataset>
   displayLegend: boolean
   labels: Array<string>
+  customChartOptions?: CustomChartOptions
 }
 
-type AgeGroup = {
+type AgeData = {
   group: string
   raw_count: number
 }
 
+type CallBacks = {
+  label: Function
+  title: Function
+}
+
 type CaseTotals = {
-  age_group: Array<AgeGroup>
-  gender: Gender
-  race_eth: RaceEthnicity
+  age_group: Array<AgeData>
+  gender: GenderData
+  race_eth: RaceEthnicityData
 }
 
-type CountiesDataType = {
-  [county: string]: CountyDataType
+type CountiesData = {
+  [county: string]: CountyData
 }
 
-type CountiesFormattedType = {
-  [county: string]: CountyFormattedType
-}
-
-type CountyDataType = {
+type CountyData = {
   case_totals: CaseTotals
   name: string
   source_url: string
   update_time: string
 }
 
-type CountyFormattedType = {
-  ageGroup?: AgeGroupGraphType
-  genderGroup?: GenderGroupGraphType
+type CustomChartOptions = {
+  plugins: Object
+  callbacks: CallBacks
+}
+
+type FormattedCountiesData = {
+  [county: string]: FormattedCountyData
+}
+
+type FormattedCountyData = {
+  ageGroup?: AgeGroup
+  genderGroup?: GenderGroup
   lastUpdatedAt?: string
   name?: string
   sourceUrl?: string
 }
 
-type Gender = {
+type GenderData = {
   [type: string]: number
 }
 
-type GenderGraphDatasetType = {
+type GenderDataset = {
   backgroundColor: string
   data: Array<number>
 }
 
-type GenderGroupGraphType = {
-  datasets: Array<GenderGraphDatasetType>
+type GenderGroup = {
+  chartType: string
+  datasets: Array<GenderDataset>
   displayLegend: boolean
   labels: Array<string>
   totalCount: number
+  customChartOptions?: CustomChartOptions
 }
 
-type RaceEthnicity = {
+type RaceEthnicityData = {
   African_Amer: number
   Asian: number
   Latinx_or_Hispanic: number
@@ -73,8 +94,11 @@ type RaceEthnicity = {
   White: number
 }
 
-const buildAgeGroupGraphData = (ageGroups: Array<AgeGroup>) => {
-  const formatAgeGroupLabels = () => {
+const buildAgeChartData = (
+  defaultAgeGroup: AgeGroup,
+  ageGroups: Array<AgeData>
+) => {
+  const formatAgeDataLabels = () => {
     return ageGroups.map((group) => {
       let name = group.group.replace('_to_', '-')
       if (name.match(/_and_under/)) {
@@ -87,19 +111,22 @@ const buildAgeGroupGraphData = (ageGroups: Array<AgeGroup>) => {
     })
   }
 
-  return {
-    displayLegend: false,
-    labels: formatAgeGroupLabels(),
-    datasets: [
-      {
-        backgroundColor: '#473A8C',
-        data: ageGroups.map((group) => group.raw_count)
-      }
-    ]
-  }
+  const updatedAgeGroup: AgeGroup = { ...defaultAgeGroup }
+
+  updatedAgeGroup.chartType = ChartTypes.AGE
+  updatedAgeGroup.displayLegend = false
+  updatedAgeGroup.labels = formatAgeDataLabels()
+  updatedAgeGroup.datasets = [
+    {
+      backgroundColor: PURPLE_MAIN,
+      data: ageGroups.map((group) => group.raw_count)
+    }
+  ]
+
+  return updatedAgeGroup
 }
 
-const getGenderTotalCount = (genders: Gender): number => {
+const getGenderTotalCount = (genders: GenderData): number => {
   let total = 0
   const genderTypes = Object.keys(genders)
   for (const gender of genderTypes) {
@@ -109,71 +136,163 @@ const getGenderTotalCount = (genders: Gender): number => {
   return total
 }
 
-const buildGenderGroupGraphData = (genders: Gender) => {
+const buildGenderChartData = (
+  defaultGenderGroup: GenderGroup,
+  genders: GenderData
+) => {
+  const updatedGenderGroup: GenderGroup = { ...defaultGenderGroup }
   const genderLabels: string[] = []
   for (const gender in genders) {
     if (genders[gender] >= 0) genderLabels.push(gender)
   }
   const genderTotalCount = getGenderTotalCount(genders)
-  const getGenderPercentageData = () => {
+  // const getGenderPercentageData = () => {
+  //   return genderLabels.map((gender) => {
+  //     return Math.round((genders[gender] / genderTotalCount) * 100)
+  //   })
+  // }
+  const getGenderCountData = () => {
     return genderLabels.map((gender) => {
-      return Math.round((genders[gender] / genderTotalCount) * 100)
+      return genders[gender]
     })
   }
 
+  updatedGenderGroup.chartType = ChartTypes.GENDER
+  updatedGenderGroup.displayLegend = false
+  updatedGenderGroup.totalCount = genderTotalCount
+  updatedGenderGroup.labels = genderLabels
+  updatedGenderGroup.datasets = [
+    {
+      backgroundColor: PURPLE_MAIN,
+      data: getGenderCountData()
+    }
+  ]
+
+  return updatedGenderGroup
+}
+
+const getCountyShortName = (countyName: string): string => {
+  return countyName
+    .replace(' County', '')
+    .replace(' ', '_')
+    .toLocaleLowerCase()
+}
+
+const parseDateForYrMoDay = (date: string): string => {
+  return date.split('T')[0]
+}
+
+const getPercentageData = (target: number, total: number) => {
+  return Math.round((target / total) * 100)
+}
+
+// const defaultCallbacks = {
+//   label(tooltipItem: any) {
+//     return tooltipItem
+//   },
+//   title(tooltipItem: any) {
+//     return tooltipItem
+//   }
+// }
+
+const getDefaultFormattedData = () => {
   return {
-    displayLegend: false,
-    totalCount: genderTotalCount,
-    labels: genderLabels,
-    datasets: [
-      {
-        backgroundColor: '#473A8C',
-        data: getGenderPercentageData()
+    ageGroup: {
+      chartType: ChartTypes.AGE,
+      datasets: [],
+      displayLegend: false,
+      labels: [],
+      customChartOptions: {
+        plugins: {},
+        callbacks: {
+          label(tooltipItem: any) {
+            if (!tooltipItem) return
+            return 'cases'
+          },
+          title(tooltipItem: any) {
+            return tooltipItem[0].value
+          }
+        }
       }
-    ]
+    },
+    genderGroup: {
+      chartType: ChartTypes.GENDER,
+      datasets: [],
+      displayLegend: false,
+      labels: [],
+      totalCount: 0,
+      customChartOptions: {
+        plugins: {},
+        callbacks: {
+          label(tooltipItem: any) {
+            if (!tooltipItem) return
+            return 'cases'
+          },
+          title(tooltipItem: any) {
+            return tooltipItem[0].value
+          }
+        }
+      }
+    }
   }
 }
 
 export default (
-  data: CountiesDataType,
+  data: CountiesData,
   allCounties: Array<string>
-): CountyFormattedType => {
-  const finalData: CountiesFormattedType = {}
+): FormattedCountyData => {
+  const finalData: FormattedCountiesData = {}
 
   for (const countyName of allCounties) {
-    const countyNameIndex = countyName
-      .replace(' County', '')
-      .replace(' ', '_')
-      .toLocaleLowerCase()
+    const defaultFormattedData = getDefaultFormattedData()
+    finalData[countyName] = defaultFormattedData
+    finalData[countyName].name = countyName
+    finalData[countyName].lastUpdatedAt = parseDateForYrMoDay(
+      new Date().toISOString()
+    )
 
+    const countyNameIndex = getCountyShortName(countyName)
     const county = data[countyNameIndex]
 
-    finalData[countyName] = {
-      lastUpdatedAt: new Date().toISOString().split('T')[0],
-      name: countyName,
-      ageGroup: {
-        datasets: [],
-        displayLegend: false,
-        labels: []
-      },
-      genderGroup: {
-        datasets: [],
-        displayLegend: false,
-        labels: [],
-        totalCount: 0
-      }
-    }
     if (county) {
-      const { case_totals, update_time, source_url } = county
+      const {
+        case_totals: { age_group, gender },
+        update_time,
+        source_url
+      } = county
 
-      finalData[countyName].lastUpdatedAt = update_time?.split('T')[0]
-      finalData[countyName].sourceUrl = source_url
-      finalData[countyName].ageGroup = buildAgeGroupGraphData(
-        case_totals.age_group
-      )
-      finalData[countyName].genderGroup = buildGenderGroupGraphData(
-        case_totals.gender
-      )
+      if (update_time) {
+        finalData[countyName].lastUpdatedAt = parseDateForYrMoDay(update_time)
+      }
+      if (source_url) finalData[countyName].sourceUrl = source_url
+      if (age_group) {
+        finalData[countyName].ageGroup = buildAgeChartData(
+          defaultFormattedData.ageGroup,
+          age_group
+        )
+      }
+      if (gender) {
+        finalData[countyName].genderGroup = buildGenderChartData(
+          defaultFormattedData.genderGroup,
+          gender
+        )
+        finalData[countyName].genderGroup!.customChartOptions!.plugins = {
+          datalabels: {
+            color: '#FFFFFF',
+            anchor: 'end',
+            align: 'left',
+            font: {
+              size: 36
+            },
+            formatter(value: any) {
+              const { totalCount } = finalData[countyName].genderGroup!
+              const percentValue = getPercentageData(value, totalCount)
+
+              return percentValue < 5 ? percentValue : `${percentValue}%`
+            }
+          }
+        }
+      }
     }
   }
 
