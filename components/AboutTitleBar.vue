b13d66a8566ef35577b4298b20094d4f816737c7
Jensen David <feudavid92@gmail.com>
2020-07-26 20:29:31 -0700

    About title bar css updated

--- components/AboutTitleBar.vue
+++ components/AboutTitleBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="AbourTitleBar">
+  <div class="AboutTitleBar">
     <div class="Background-Wrapper">
       <div class="header-container">
         <h2>
@@ -23,21 +23,23 @@ export default {
 }
 </script>
 <style lang="scss">
-.Background-Wrapper {
-  background: url('../static/titlebar_bg.png') center no-repeat;
-  height: 340px;
-  position: relative;
-  width: 1440px;
-  height: 340px;
-  left: 0px;
-  right: 0px;
-  top: 0px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
-  @include lessThan($small) {
-    display: none;
+.AboutTitleBar {
+  .Background-Wrapper {
+    background: url('../static/titlebar_bg.png') center no-repeat;
+    height: 340px;
+    position: relative;
+    width: 1440px;
+    height: 340px;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    margin-bottom: 30px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+    @include lessThan($small) {
+      display: none;
+    }
   }
 }
-
 .header-container {
   position: absolute;
   width: 1000px;
