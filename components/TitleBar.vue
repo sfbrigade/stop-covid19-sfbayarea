8a4a0905a39e104a6022c76aefa2895a7fde1585
Thuong Ho <thuong.t.ho@gmail.com>
2020-07-31 11:22:47 -0700

    Fix About page top navigation not responsive #272
    Fix TitleBar titleId type mismatch
    Set TopNavigation default to null so that no tab is preselected for About page

--- components/TitleBar.vue
+++ components/TitleBar.vue
@@ -42,8 +42,8 @@ export default {
       default: ''
     },
     titleId: {
-      type: String,
-      default: ''
+      type: Number,
+      default: null
     },
     subheader: {
       type: String,
