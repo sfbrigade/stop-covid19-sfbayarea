e0fc061c718b56284336d4b4e14fe917e066e42e
Jensen David <feudavid92@gmail.com>
2020-08-23 18:49:47 -0700

    added countyguidelines.vue and temporarily disabled all county news from showing. Only San Francisco is showing

--- components/News.vue
+++ components/News.vue
@@ -3,35 +3,39 @@
     <div v-for="(county, i) in counties" :key="i" class="News-county">
       <whats-new class="mb-4" :feed="county" />
     </div>
+    <CityGuidelines />
   </div>
 </template>
 <script>
 import WhatsNew from '@/components/WhatsNew.vue'
-import AlamedaNews from '@/data/news/alameda.json'
-import ContraCostaNews from '@/data/news/contra_costa.json'
-import MarinNews from '@/data/news/marin.json'
-import NapaNews from '@/data/news/napa.json'
+// import AlamedaNews from '@/data/news/alameda.json'
+// import ContraCostaNews from '@/data/news/contra_costa.json'
+// import MarinNews from '@/data/news/marin.json'
+// import NapaNews from '@/data/news/napa.json'
 import SanFranciscoNews from '@/data/news/san_francisco.json'
-import SanMateoNews from '@/data/news/san_mateo.json'
-import SantaClaraNews from '@/data/news/santa_clara.json'
-import SolanoNews from '@/data/news/solano.json'
-import SonomaNews from '@/data/news/sonoma.json'
+// import SanMateoNews from '@/data/news/san_mateo.json'
+// import SantaClaraNews from '@/data/news/santa_clara.json'
+// import SolanoNews from '@/data/news/solano.json'
+// import SonomaNews from '@/data/news/sonoma.json'
+import CityGuidelines from '@/components/CityGuidelines.vue'
+
 export default {
   components: {
-    WhatsNew
+    WhatsNew,
+    CityGuidelines
   },
   data() {
     const data = {
       counties: [
-        SanFranciscoNews,
-        AlamedaNews,
-        ContraCostaNews,
-        MarinNews,
-        NapaNews,
-        SanMateoNews,
-        SantaClaraNews,
-        SolanoNews,
-        SonomaNews
+        SanFranciscoNews
+        // AlamedaNews,
+        // ContraCostaNews,
+        // MarinNews,
+        // NapaNews,
+        // SanMateoNews,
+        // SantaClaraNews,
+        // SolanoNews,
+        // SonomaNews
       ]
     }
     return data
