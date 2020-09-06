781b363c1be55ac718e44ce2f6c9cf525194a9a5
Jensen David <feudavid92@gmail.com>
2020-09-05 17:44:34 -0700

    json is showing but needs more styling updates

--- components/CityGuidelines.vue
+++ components/CityGuidelines.vue
@@ -2,9 +2,26 @@
   <div class="CityGuidelines">
     <div v-for="(item, i) in info[county].info" :key="i">
       <v-card class="guidelines">
-        <header>{{ item.header }}</header>
+        <h2>{{ item.header }}</h2>
         <div v-for="(body, j) in item.body" :key="j">
           <h4>{{ body.h4 }}</h4>
+          <div v-for="(content, k) in body.content" :key="k">
+            <div v-for="(p, l) in content.p2" :key="l" class="p2">
+              <p>{{ p.p }}</p>
+            </div>
+            <div
+              v-for="(bullet, m) in content.bullets"
+              :key="m"
+              class="bullets"
+            >
+              <ul>
+                <li>{{ bullet.bullet }}</li>
+              </ul>
+            </div>
+            <div class="link-container">
+              <a href="">{{ content.link }}</a>
+            </div>
+          </div>
         </div>
       </v-card>
     </div>
