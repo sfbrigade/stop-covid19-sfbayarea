2cf3c42f57685f4da895b9fd418c1c2b694abce0
Thuong Ho <thuong.t.ho@gmail.com>
2020-10-25 16:06:18 -0700

    Create a dropdown component
    Update News and Stats to use dropdown component

---
+++ components/DropDown.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="dropdown-container">
+    <select v-model="selectedOption" @change="handleSelect($event)">
+      <option v-for="(option, index) in dropdownOptions" :key="index">
+        {{ option }}
+      </option>
+    </select>
+    <v-icon class="dropdown-icon">
+      fa fa-chevron-down
+    </v-icon>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    dropdownOptions: {
+      type: Array,
+      required: true
+    },
+    dropdownModel: {
+      type: String,
+      default: '',
+      required: false
+    }
+  },
+  data() {
+    const selectedOption = this.dropdownModel
+    return {
+      selectedOption
+    }
+  },
+  methods: {
+    handleSelect(event) {
+      const dropdownModel = event.target.value
+      this.$emit('selectedOption', dropdownModel)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.dropdown-container {
+  border: 1px solid $gray-5;
+  border-radius: 5px;
+  background: $white-1;
+
+  select {
+    padding: 0.5em 0.5em 0.5em 1em;
+  }
+
+  .dropdown-icon {
+    font-size: 12px;
+    margin-right: 1em;
+  }
+
+  select:focus {
+    outline: none;
+  }
+}
+</style>
