2713b158e06f6ea66eba09acb971b62b34419c51
Kabirdas Henry <kbrdsk@gmail.com>
2021-01-11 10:36:00 -0800

    removed duplicate code

--- components/TimeLineChartCountyComparison.vue
+++ components/TimeLineChartCountyComparison.vue
@@ -76,95 +76,75 @@ export default {
     }
   },
   data() {
+    const caseData = {}
+    const percentData = {}
+    for (const county in this.chartData) {
+      const confirmedDailyIn14daysQueue = []
+      caseData[county] = this.chartData[county].graph.map(d => {
+        // calculate new cases per 100,000 residents (14 day average)
+        confirmedDailyIn14daysQueue.push(d.confirmedTransition)
+        if (confirmedDailyIn14daysQueue.length > 14) {
+          confirmedDailyIn14daysQueue.shift()
+        }
+        const averageDailyCases =
+          confirmedDailyIn14daysQueue.reduce((pre, curr) => {
+            return pre + curr
+          }, 0) / confirmedDailyIn14daysQueue.length
+        return averageDailyCases / (this.chartData[county].population / 100000)
+      })
+
+      const confirmedCumulativeIn7daysQueue = []
+      percentData[county] = this.chartData[county].graph.map(d => {
+        confirmedCumulativeIn7daysQueue.push(d.cumulative)
+        if (confirmedCumulativeIn7daysQueue.length > 7) {
+          const confirmedCumulative7daysBefore = confirmedCumulativeIn7daysQueue.shift()
+          return (
+            ((d.cumulative - confirmedCumulative7daysBefore) /
+              confirmedCumulative7daysBefore) *
+            100
+          )
+        } else {
+          return null
+        }
+      })
+    }
     return {
-      timePickerSelected: '30'
+      timePickerSelected: '30',
+      caseData,
+      percentData
     }
   },
   computed: {
     displayData() {
       if (this.selectedCounties.length) {
         const dataSets = []
-        if (this.chartDataType === 'casesperpeople') {
-          for (const county of this.selectedCounties) {
-            const confirmedDailyIn14daysQueue = []
-            dataSets.push({
-              type: 'line',
-              fill: false,
-              borderWidth: 3,
-              pointBackgroundColor: 'rgba(0,0,0,0)',
-              pointBorderColor: 'rgba(0,0,0,0)',
-              borderColor: county.color,
-              lineTension: 0.5,
-              borderJoinStyle: 'round',
-              label: county.name,
-              data: this.sliceToTimePick(this.chartData[county.name].graph).map(
-                d => {
-                  // calculate new cases per 100,000 residents (14 day average)
-                  confirmedDailyIn14daysQueue.push(d.confirmedTransition)
-                  if (confirmedDailyIn14daysQueue.length > 14) {
-                    confirmedDailyIn14daysQueue.shift()
-                  }
-                  const averageDailyCases =
-                    confirmedDailyIn14daysQueue.reduce((pre, curr) => {
-                      return pre + curr
-                    }, 0) / confirmedDailyIn14daysQueue.length
-                  return (
-                    averageDailyCases /
-                    (this.chartData[county.name].population / 100000)
-                  )
-                }
-              )
-            })
+        const data =
+          this.chartDataType === 'casesperpeople'
+            ? this.caseData
+            : this.percentData
+        const labels = this.chartData[this.selectedCounties[0].name].graph.map(
+          d => {
+            return d.label
           }
+        )
+        for (const county of this.selectedCounties) {
+          dataSets.push({
+            type: 'line',
+            fill: false,
+            borderWidth: 3,
+            pointBackgroundColor: 'rgba(0,0,0,0)',
+            pointBorderColor: 'rgba(0,0,0,0)',
+            borderColor: county.color,
+            lineTension: 0.5,
+            borderJoinStyle: 'round',
+            label: county.name,
+            data: this.sliceToTimePick(data[county.name])
+          })
+        }
 
-          return {
-            labels: this.sliceToTimePick(
-              this.chartData[this.selectedCounties[0].name].graph
-            ).map(d => {
-              return d.label
-            }),
-            datasets: dataSets
-          }
-        } else {
-          // Percent Increase in 7 days
-          for (const county of this.selectedCounties) {
-            const confirmedCumulativeIn7daysQueue = []
-            dataSets.push({
-              type: 'line',
-              fill: false,
-              borderWidth: 3,
-              pointBackgroundColor: 'rgba(0,0,0,0)',
-              pointBorderColor: 'rgba(0,0,0,0)',
-              borderColor: county.color,
-              lineTension: 0.5,
-              borderJoinStyle: 'round',
-              label: county.name,
-              data: this.sliceToTimePick(
-                this.chartData[county.name].graph.map(d => {
-                  confirmedCumulativeIn7daysQueue.push(d.cumulative)
-                  if (confirmedCumulativeIn7daysQueue.length > 7) {
-                    const confirmedCumulative7daysBefore = confirmedCumulativeIn7daysQueue.shift()
-                    return (
-                      ((d.cumulative - confirmedCumulative7daysBefore) /
-                        confirmedCumulative7daysBefore) *
-                      100
-                    )
-                  } else {
-                    return null
-                  }
-                })
-              )
-            })
-          }
-
-          return {
-            labels: this.sliceToTimePick(
-              this.chartData[this.selectedCounties[0].name].graph
-            ).map(d => {
-              return d.label
-            }),
-            datasets: dataSets
-          }
+        return {
+          labels: this.sliceToTimePick(labels),
+          datasets: dataSets
         }
       } else {
         return {
