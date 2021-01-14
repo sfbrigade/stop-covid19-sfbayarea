880edae77c7fbaefa79d4c727e18fb7c723eb14b
Kabirdas Henry <52506986+kbrdsk@users.noreply.github.com>
2021-01-13 19:23:34 -0800

    Add date range selection to county comparison chart (#835)
    * added time picker to county comparison charts
    
    * removed duplicate code
    
    * sliceToTimePick is only used in one place so defining it there

--- components/TimeLineChartCountyComparison.vue
+++ components/TimeLineChartCountyComparison.vue
@@ -6,6 +6,13 @@
     :url="url"
     :chart-info="chartInfo"
   >
+    <template v-slot:button>
+      <TimePickerDropdown
+        class="dropdown-container"
+        :time-picker-model="timePickerSelected"
+        @timePickerSelected="handleTimePick"
+      />
+    </template>
     <bar
       v-if="displayData != null"
       :chart-data="displayData"
@@ -17,9 +24,10 @@
 
 <script>
 import DataView from '@/components/DataView.vue'
+import TimePickerDropdown from '@/components/TimePickerDropdown'
 
 export default {
-  components: { DataView },
+  components: { DataView, TimePickerDropdown },
   props: {
     title: {
       type: String,
@@ -68,89 +76,78 @@ export default {
     }
   },
   data() {
-    return {}
+    const caseData = {}
+    const percentData = {}
+    for (const county in this.chartData) {
+      const confirmedDailyIn14daysQueue = []
+      caseData[county] = this.chartData[county].graph.map(d => {
+        // calculate new cases per 100,000 residents (14 day average)
+        confirmedDailyIn14daysQueue.push(d.confirmedTransition)
+        if (confirmedDailyIn14daysQueue.length > 14) {
+          confirmedDailyIn14daysQueue.shift()
+        }
+        const averageDailyCases =
+          confirmedDailyIn14daysQueue.reduce((pre, curr) => {
+            return pre + curr
+          }, 0) / confirmedDailyIn14daysQueue.length
+        return averageDailyCases / (this.chartData[county].population / 100000)
+      })
+
+      const confirmedCumulativeIn7daysQueue = []
+      percentData[county] = this.chartData[county].graph.map(d => {
+        confirmedCumulativeIn7daysQueue.push(d.cumulative)
+        if (confirmedCumulativeIn7daysQueue.length > 7) {
+          const confirmedCumulative7daysBefore = confirmedCumulativeIn7daysQueue.shift()
+          return (
+            ((d.cumulative - confirmedCumulative7daysBefore) /
+              confirmedCumulative7daysBefore) *
+            100
+          )
+        } else {
+          return null
+        }
+      })
+    }
+    return {
+      timePickerSelected: '30',
+      caseData,
+      percentData
+    }
   },
   computed: {
     displayData() {
       if (this.selectedCounties.length) {
         const dataSets = []
-        if (this.chartDataType === 'casesperpeople') {
-          for (const county of this.selectedCounties) {
-            const confirmedDailyIn14daysQueue = []
-            dataSets.push({
-              type: 'line',
-              fill: false,
-              borderWidth: 3,
-              pointBackgroundColor: 'rgba(0,0,0,0)',
-              pointBorderColor: 'rgba(0,0,0,0)',
-              borderColor: county.color,
-              lineTension: 0.5,
-              borderJoinStyle: 'round',
-              label: county.name,
-              data: this.chartData[county.name].graph.map(d => {
-                // calculate new cases per 100,000 residents (14 day average)
-                confirmedDailyIn14daysQueue.push(d.confirmedTransition)
-                if (confirmedDailyIn14daysQueue.length > 14) {
-                  confirmedDailyIn14daysQueue.shift()
-                }
-                const averageDailyCases =
-                  confirmedDailyIn14daysQueue.reduce((pre, curr) => {
-                    return pre + curr
-                  }, 0) / confirmedDailyIn14daysQueue.length
-                return (
-                  averageDailyCases /
-                  (this.chartData[county.name].population / 100000)
-                )
-              })
-            })
+        const data =
+          this.chartDataType === 'casesperpeople'
+            ? this.caseData
+            : this.percentData
+        const labels = this.chartData[this.selectedCounties[0].name].graph.map(
+          d => {
+            return d.label
           }
+        )
+        const sliceToTimePick = arr =>
+          arr.slice(-Number(this.timePickerSelected) || 0)
 
-          return {
-            labels: this.chartData[this.selectedCounties[0].name].graph.map(
-              d => {
-                return d.label
-              }
-            ),
-            datasets: dataSets
-          }
-        } else {
-          // Percent Increase in 7 days
-          for (const county of this.selectedCounties) {
-            const confirmedCumulativeIn7daysQueue = []
-            dataSets.push({
-              type: 'line',
-              fill: false,
-              borderWidth: 3,
-              pointBackgroundColor: 'rgba(0,0,0,0)',
-              pointBorderColor: 'rgba(0,0,0,0)',
-              borderColor: county.color,
-              lineTension: 0.5,
-              borderJoinStyle: 'round',
-              label: county.name,
-              data: this.chartData[county.name].graph.map(d => {
-                confirmedCumulativeIn7daysQueue.push(d.cumulative)
-                if (confirmedCumulativeIn7daysQueue.length > 7) {
-                  const confirmedCumulative7daysBefore = confirmedCumulativeIn7daysQueue.shift()
-                  return (
-                    ((d.cumulative - confirmedCumulative7daysBefore) /
-                      confirmedCumulative7daysBefore) *
-                    100
-                  )
-                } else {
-                  return null
-                }
-              })
-            })
-          }
+        for (const county of this.selectedCounties) {
+          dataSets.push({
+            type: 'line',
+            fill: false,
+            borderWidth: 3,
+            pointBackgroundColor: 'rgba(0,0,0,0)',
+            pointBorderColor: 'rgba(0,0,0,0)',
+            borderColor: county.color,
+            lineTension: 0.5,
+            borderJoinStyle: 'round',
+            label: county.name,
+            data: sliceToTimePick(data[county.name])
+          })
+        }
 
-          return {
-            labels: this.chartData[this.selectedCounties[0].name].graph.map(
-              d => {
-                return d.label
-              }
-            ),
-            datasets: dataSets
-          }
+        return {
+          labels: sliceToTimePick(labels),
+          datasets: dataSets
         }
       } else {
         return {
@@ -246,8 +243,17 @@ export default {
         }
       }
     }
+  },
+  methods: {
+    handleTimePick(timePickerSelected) {
+      this.timePickerSelected = timePickerSelected
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dropdown-container {
+  margin-top: 10px;
+}
+</style>
